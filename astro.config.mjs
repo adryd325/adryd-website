f06ae708f538919149d57579f05598e23ec09488
adryd <me@adryd.com>
2025-07-28 22:28:06 -0400

    fancy error

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://adryd.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap()]
 });
